f782d526b1f1638452a4d521b5787b17afc54645
Gabriel <53164692+Gabldn@users.noreply.github.com>
2023-07-28 16:19:19 +0800

    debugged generate QR code button, needed to add event listener

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function generateQRCode() {
   const inputLink = document.getElementById("inputLink").value;
   const qrcodeDiv = document.getElementById("qrcode");
-
   if (!inputLink) {
     alert("Please enter a link or text.");
     return;
@@ -10,3 +9,37 @@ function generateQRCode() {
   // Generate the QR code using qrcode.js library
   new QRCode(qrcodeDiv, inputLink);
 }
+
+// Event listener for the button click
+generateQR.addEventListener("click", () => {
+  // Increment the number when the button is clicked
+  generateQRCode()
+  number++;
+  // Update the number display
+  updateNumberDisplay();
+});
+
+
+
+// Access the HTML elements
+const numberDisplayElement = document.getElementById("numberDisplay");
+const incrementButton = document.getElementById("incrementButton");
+
+// Initial number
+let number = 0;
+
+// Function to update the number display
+function updateNumberDisplay() {
+  numberDisplayElement.textContent = number;
+}
+
+// Event listener for the button click
+incrementButton.addEventListener("click", () => {
+  // Increment the number when the button is clicked
+  number++;
+  // Update the number display
+  updateNumberDisplay();
+});
+
+// Initial update to display the starting number (0)
+updateNumberDisplay();
